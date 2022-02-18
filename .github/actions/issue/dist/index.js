85935d00efb7ed2edb93e7e2b44bc5f8166d8493
kenro17 <kento_1st@icloud.com>
2022-02-18 23:54:43 +0900

    feat: b

--- .github/actions/issue/dist/index.js
+++ .github/actions/issue/dist/index.js
@@ -4308,7 +4308,7 @@ async function run() {
 
     const octokit = new github.getOctokit(token);
 
-    const response = await octokit.issues.create({
+    const response = await octokit.rest.issues.create({
       // owner: github.context.repo.owner,
       // repo: github.context.repo.repo,
       ...github.context.repo,
